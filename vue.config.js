6cf65e801a1ab30674c0fe2436b2f4a957ed3c8d
pro100 <pro100.sto@yandex.ru>
2023-03-25 18:45:45 +0300

    update github pages

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  process.env.NODE_ENV === "production" ? "/test-routeam/" : "/",
-  transpileDependencies: true
-})
+  publicPath: process.env.NODE_ENV === "production" ? "/test-routeam/" : "/",
+  transpileDependencies: true,
+});
